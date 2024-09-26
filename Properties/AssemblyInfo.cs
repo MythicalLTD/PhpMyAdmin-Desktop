8c6f6bcc6e4b4054053b3080703d5c85cc66baae
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2024-09-26 11:15:05 +0200

    PUSH
    -> Publish APP

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PhpMyAdmin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A phpmyadmin desktop app!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MythicalSystems")]
+[assembly: AssemblyCompany("MythicalSystems LTD")]
 [assembly: AssemblyProduct("PhpMyAdmin")]
 [assembly: AssemblyCopyright("Copyright © MythicalSystems 2024")]
 [assembly: AssemblyTrademark("")]
